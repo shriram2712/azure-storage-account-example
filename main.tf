df69c7aff6c98360d6b286e11d0a37f541b115ab
Shriram Rajaraman <32219070+shriram2712@users.noreply.github.com>
2024-05-21 18:01:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,4 +3,6 @@ module "storage-container" {
   #version = "1.0.0"
 
   prefix = var.prefix
+  resource_group_name = var.resource_group_name
+  storage_account_container_name = var.storage_account_container_name
 }
