30b2209e02ac5870b0fb1fc03f41b262cd34e6c3
Shriram Rajaraman <32219070+shriram2712@users.noreply.github.com>
2024-05-21 18:01:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,15 @@ variable "prefix" {
   description = "Prefix for bucket"
   default = "shriram"
 }
+
+variable "resource_group_name" {
+  type        = string
+  description = "Azure resource group name"
+  default = "Terraform-POC-resources"
+}
+
+variable "storage_account_container_name" {
+  type        = string
+  description = "Storage account container name"
+  default = "test-storage-container"
+}
